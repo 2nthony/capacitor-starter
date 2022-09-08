2a0ae1ba1031f36fecbc1b3a0773c46605814511
2nthony <ant.hu@outlook.com>
2022-09-08 20:47:25 +0800

    chore(capacitor): change appId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ import { ip } from "address";
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "myAppName.2nthony.com",
+  appId: "com.capacitorjs.app",
   appName: "capacitor-app",
   bundledWebRuntime: false,
   webDir: "dist",
