fd89175e389c9f2e4a47966ff9482d042fd7d06b
2nthony <ant.hu@outlook.com>
2022-10-18 23:11:27 +0800

    fix(cap): app_env

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,22 +1,26 @@
 /// <reference types="@capacitor/splash-screen" />
 
+const isDev = process.env.APP_ENV === "dev";
+
 import { ip } from "address";
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.capacitorjs.app",
   appName: "capacitor-app",
-  bundledWebRuntime: false,
   webDir: "dist",
   plugins: {
     SplashScreen: {
       launchShowDuration: 6000,
     },
   },
-  server: {
+};
+
+if (isDev) {
+  config.server = {
     url: `http://${ip()}:5173`,
     cleartext: true,
-  },
-};
+  };
+}
 
 export default config;
